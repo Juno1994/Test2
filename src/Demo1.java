6e5a894c448ceb10b1edb1d7564d1501491091c2
Juno1994 <43963934+Juno1994@users.noreply.github.com>
2018-10-10 14:29:21 +0800

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -4,6 +4,7 @@ public class Demo1 {
     public void show(){
         System.out.println("hello gitHub again!");
         System.out.println("hello gitHub again!");
+        System.out.println("hello gitHub again!");
     }
     
 }
